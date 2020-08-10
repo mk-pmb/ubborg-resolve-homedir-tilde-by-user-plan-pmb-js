770da41ff705d4d20e1651739ccdf539ec39644a
mk-pmb <mk-pmb@users.noreply.github.com>
2020-08-10 06:10:10 +0200

    v0.2.0: Adapt to planner v0.0.67

--- reso.mjs
+++ reso.mjs
@@ -2,6 +2,7 @@
 
 import mustBe from 'typechecks-pmb/must-be';
 import splitOnce from 'split-string-or-buffer-once-pmb';
+import getOwn from 'getown';
 
 const hdResType = 'osUser';
 const hdProp = 'homeDirPath';
@@ -12,8 +13,8 @@ async function resolveHomeDirTildeByUserPlan(spawnCtx, path, defaultUserName) {
   if (!path.startsWith('~')) { return path; }
   const [hduExplicitName, subPath] = (splitOnce('/', path.slice(1)) || []);
   const hduName = (hduExplicitName || defaultUserName || '');
-  const hduPlan = (spawnCtx.getResourcesByTypeName()[hdResType]
-    || false)[hduName];
+  const userPlans = getOwn(spawnCtx.getResPlanPrByTypeName(), hdResType);
+  const hduPlan = await getOwn(userPlans, hduName);
   if (!hduPlan) {
     const fail = ('Cannot lookup ' + hdProp + ' of ' + hdResType
       + '[' + hduName + ']: Resource is not planned yet.');
