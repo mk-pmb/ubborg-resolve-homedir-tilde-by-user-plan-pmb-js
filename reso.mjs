41d31fe099a3e943a77b95751a1fb4444a622fa1
mk-pmb <mk-pmb@users.noreply.github.com>
2022-12-28 21:42:10 +0100

    v0.2.2: Support homeDirPaths override

--- reso.mjs
+++ reso.mjs
@@ -1,31 +1,65 @@
 // -*- coding: utf-8, tab-width: 2 -*-
 
+import getOwn from 'getown';
+import isStr from 'is-string';
 import mustBe from 'typechecks-pmb/must-be';
 import splitOnce from 'split-string-or-buffer-once-pmb';
-import getOwn from 'getown';
 
 const hdResType = 'osUserLogin';
 const hdProp = 'homeDirPath';
+const preferredUserNameProp = 'defaultUserName';
 
-async function resolveHomeDirTildeByUserPlan(spawnCtx, path, defaultUserName) {
-  mustBe.nest('path', path);
-  mustBe('undef | nonEmpty str', 'defaultUserName')(defaultUserName);
+
+const EX = async function resolveHomeDirTildeByUserPlan(spawnCtx, path, opt) {
+  mustBe.nest('path  ', path);
+
+  const nopt = EX.normalizeOptionsObject(opt);
+  const pun = nopt[preferredUserNameProp];
+  const dun = (pun
+    || nopt.enforcedOwner
+    || nopt.owner
+    || pun); // <-- Repeat in order to ensure correct error message.
+  mustBe('undef | nonEmpty str', preferredUserNameProp)(dun);
+
+  // Check path only when opt was ok, in order to not conceal opt errors.
   if (!path.startsWith('~')) { return path; }
-  const [hduExplicitName, subPath] = (splitOnce('/', path.slice(1)) || []);
-  const hduName = (hduExplicitName || defaultUserName || '');
-  const userPlans = getOwn(spawnCtx.getResPlanPrByTypeName(), hdResType);
-  const hduPlan = await getOwn(userPlans, hduName);
-  if (!hduPlan) {
-    const fail = ('Cannot lookup ' + hdProp + ' of ' + hdResType
-      + '[' + hduName + ']: Resource is not planned yet.');
-    throw new Error(fail);
-  }
-  await hduPlan.hatchedPr;
-  const facts = await hduPlan.toFactsDict();
-  const hdPath = facts[hdProp];
-  mustBe.nest('home directory path for ' + String(hduPlan), hdPath);
-  const reso = (hdPath + (subPath === undefined ? '' : '/') + (subPath || ''));
-  return reso;
-}
 
-export default resolveHomeDirTildeByUserPlan;
+  let [hduName, subPath] = (splitOnce('/', path.slice(1)) || []);
+  hduName = (hduName || dun || '');
+  const hdPath = await EX.findHomeDirPath(spawnCtx, hduName, nopt);
+  mustBe.nest('home directory path for ' + hduName, hdPath);
+  subPath = (subPath || '');
+  const reso = (hdPath + (subPath && '/') + subPath);
+  return reso;
+};
+
+
+Object.assign(EX, {
+
+  normalizeOptionsObject(opt) {
+    if (isStr(opt)) { return { defaultUserName: opt }; }
+    if (!opt) { return false; }
+    return opt;
+  },
+
+  async findHomeDirPath(spawnCtx, hduName, opt) {
+    const pathOverride = getOwn(opt.homeDirPaths, hduName);
+    if (pathOverride) { return pathOverride; }
+
+    const userPlans = getOwn(spawnCtx.getResPlanPrByTypeName(), hdResType);
+    const hduPlan = await getOwn(userPlans, hduName);
+    if (!hduPlan) {
+      const fail = ('Cannot lookup ' + hdProp + ' of ' + hdResType
+        + '[' + hduName + ']: Resource is not planned yet.');
+      throw new Error(fail);
+    }
+    await hduPlan.hatchedPr;
+    const facts = await hduPlan.toFactsDict();
+    const hdPath = facts[hdProp];
+    return hdPath;
+  },
+
+});
+
+
+export default EX;
